956cdb7f89218fa5415203d399ceb290aedad744
MuradNuh-zada <muradnuhzada@gmail.com>
2026-05-01 08:08:35 +0400

    Refactor carousel implementation and styles
    - Updated the structure of the carousel in index.html to use a new wrapper class for better styling.
    - Enhanced the SCSS for the carousel, including new styles for hero elements and improved layout.
    - Removed commented-out code related to the previous carousel implementation for cleaner code.
    - Adjusted styles in style.scss to accommodate the new carousel structure and improve responsiveness.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,54 @@
+const destinationHeadSlider = [
+  {
+    id: 1,
+    city: "MOSKVA",
+    description: "Gəz, gəz və yenə gəz",
+    image: "https://bravotravel.az/img/moskva_releated.jpg",
+  },
+  {
+    id: 2,
+    city: "İSTANBUL",
+    description: "Tarix yazan şəhər",
+    image: "https://bravotravel.az/img/istanbul_releated.jpg",
+  },
+  {
+    id: 3,
+    city: "DUBAY",
+    description: "Dünyanın günəşli bucağı",
+    image: "https://bravotravel.az/img/dubai_releated2.jpg",
+  },
+  {
+    id: 4,
+    city: "AVROPA",
+    description: "İlk qərar verilən destinasiya",
+    image: "https://bravotravel.az/img/europe_releated3.jpg",
+  },
+  {
+    id: 5,
+    city: "MALDİV",
+    description: "Bir yerüzü cənnəti",
+    image: "https://bravotravel.az/img/maldiv_releated1.jpg",
+  },
+];
+
+const swiperWrapper = document.querySelector(
+  ".hero-slider-engine .swiper-wrapper",
+);
+swiperWrapper.innerHTML = destinationHeadSlider
+  .map(
+    (item) => `
+    <div class="swiper-slide hero-item-card" data-title="${item.city}" data-desc="${item.description}">
+        <div class="hero-overlay"></div>
+        <img src="${item.image}" alt="${item.city}" />
+        <div class="hero-info-content">
+            <h3>${item.city}</h3>
+            <p>${item.description}</p>
+        </div>
+    </div>
+`,
+  )
+  .join("");
+
 const body = document.body,
   header = document.querySelector("header"),
   placeholder = document.querySelector("#header-placeholder"),
@@ -103,5 +154,36 @@ const swiper = new Swiper(".cards-carousel", {
   navigation: {
     nextEl: ".card-next",
     prevEl: ".card-prev",
+  },
+});
+
+const uniqueHeroSwiper = new Swiper(".hero-slider-engine", {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  allowTouchMove: false,
+  slidesPerView: "auto",
+  spaceBetween: 145,
+  pagination: {
+    el: ".hero-dots-container",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return `<div class="${className} dot-parent"><span class="dot"></span></div>`;
+    },
+  },
+  on: {
+    slideChange: function () {
+      const activeSlide = this.slides[this.activeIndex];
+      const title = activeSlide.getAttribute("data-title");
+      const desc = activeSlide.getAttribute("data-desc");
+
+      const titleEl = document.getElementById("hero-title-text");
+      const descEl = document.getElementById("hero-desc-text");
+
+      if (titleEl && title) titleEl.innerText = title;
+      if (descEl && desc) descEl.innerText = desc;
+    },
   },
 });
