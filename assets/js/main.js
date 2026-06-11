bcaa4ed2f90d52e4fd206fbea54d793c154ba4ff
MuradNuh-zada <muradnuhzada@gmail.com>
2026-06-11 16:49:54 +0400

    Detail page demo version finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,31 +49,109 @@ swiperWrapper.innerHTML = destinationHeadSlider
   )
   .join("");
 
-function setElementsWithCount(array, count) {
-  for (let i = 1; i <= count; i++) {
-    array.push({
-      id: i,
-      city: "Tbilisi",
-      time: "18.09.2022 - 23.09.2022",
-      price: "2453$ - 2345$",
-      imageSrc:
-        "https://bravotravel.az/uploads/posts/2022-08/medium/1661868740_2022-08-30-18.08.16.jpg",
-    });
-  }
-}
 // ! Set hot tours cards
-const hotToursElforSlider = [];
-setElementsWithCount(hotToursElforSlider, 5);
+const hotToursElforSlider = [
+  {
+    id: 0,
+    title: "İSTANBULDA BLACK FRİDAY",
+    date: "23.11.2022 - 27.11.2022",
+    price: "559$ - 717$",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-09/thumbs/1664460726_photo_5303063749382488715_y.jpg",
+    link: "https://bravotravel.az/new-tours/68-stanbulda-black-frday.html",
+  },
+  {
+    id: 1,
+    title: "BANGKOK-PATTAYA",
+    date: "06.11.2022 - 13.11.2022",
+    price: "2455$ - 3696$",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-09/medium/1663771518_3.png",
+    link: "https://bravotravel.az/new-tours/67-bangkok-pattaya.html",
+  },
+  {
+    id: 2,
+    title: "LAPLANDİYA YENİ İL",
+    date: "27.12.2022 - 03.01.2023",
+    price: "4570$ - 10090$",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-09/medium/1662988718_photo_5251532023095476369_y.jpg",
+    link: "https://bravotravel.az/new-tours/66-laplandya-yen-l.html",
+  },
+  {
+    id: 3,
+    title: "KAPADOKYA",
+    date: "25.09 - 30.09",
+    price: "1640$ - 2210$",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-09/thumbs/1662541308_55.png",
+    link: "https://bravotravel.az/new-tours/64-kapadokya.html",
+  },
+  {
+    id: 4,
+    title: "Dubay",
+    date: "08.11.2022 - 12.11.2022",
+    price: "2725 AZN - 3200 AZN",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-08/medium/1661871303_8787.jpg",
+    link: "https://bravotravel.az/new-tours/63-dubay.html",
+  },
+  {
+    id: 5,
+    title: "Cenevrə",
+    date: "15.10.2022 - 21.10.2022",
+    price: "2460 USD - 4153 USD",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-08/medium/1661869774_202.jpg",
+    link: "https://bravotravel.az/new-tours/62-cenevre.html",
+  },
+  {
+    id: 6,
+    title: "Disneyland Paris",
+    date: "20.10.2022 - 26.10.2022",
+    price: "4710 USD - 6480 USD",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-08/medium/1661869296_123123.jpg",
+    link: "https://bravotravel.az/new-tours/61-disneyland-paris.html",
+  },
+  {
+    id: 7,
+    title: "Tbilisi",
+    date: "18.09.2022 - 23.09.2022",
+    price: "1729 AZN - 2426 AZN",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-08/medium/1661868740_2022-08-30-18.08.16.jpg",
+    link: "https://bravotravel.az/new-tours/60-tbilisi.html",
+  },
+  {
+    id: 8,
+    title: "AMSTERDAM",
+    date: "10.11.2022 - 15.11.2022",
+    price: "2707 AZN - 5174 AZN",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-08/medium/1661433370_photo_2022-08-25_16-15-30.jpg",
+    link: "https://bravotravel.az/new-tours/59-amsterdam.html",
+  },
+  {
+    id: 9,
+    title: "Marmaris",
+    date: "02.09.2022 - 09.09.2022",
+    price: "1874 USD - 2040 USD",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-08/medium/1661432491_img_9995.jpg",
+    link: "https://bravotravel.az/new-tours/58-marmaris.html",
+  },
+];
 const hotTourCardsParent = document.querySelector(".tours-carousel .cards");
 hotTourCardsParent.innerHTML = hotToursElforSlider
   .map(
-    (element) => `<div class="card swiper-slide">
+    (element) => `<div href="${element.link}" class="card swiper-slide">
       <img
-          src="${element.imageSrc}" alt="${element.city}"
+          src="${element.image}" alt="${element.city}"
       />
         <div class="card-info">
-          <h3>${element.city}</h3>
-          <h4>${element.time}</h4>
+          <h3>${element.title}</h3>
+          <h4>${element.date}</h4>
           <div class="card-footer">
             <p>${element.price}</p>
             <span>Ətraflı</span>
@@ -84,45 +162,216 @@ hotTourCardsParent.innerHTML = hotToursElforSlider
   .join("");
 
 // ! Set new tours cards
-const newToursElements = [];
-setElementsWithCount(newToursElements, 12);
+const newToursElements = [
+  {
+    id: 0,
+    title: "BALİ TURU",
+    date: "10.04.2023 - 20.04.2023",
+    price: "5885 AZN",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-12/medium/1671814845_320197824_474959374762484_6974740135922900451_n.jpg",
+    link: "https://bravotravel.az/new-tours/117-bal-turu.html",
+  },
+  {
+    id: 1,
+    title: "GƏMİ TURU",
+    date: "03.02.2023 - 10.02.2023",
+    price: "470 EUR",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-12/medium/1671814744_319399561_1404265310312231_1180767154768122051_n.jpg",
+    link: "https://bravotravel.az/new-tours/116-gem-turu.html",
+  },
+  {
+    id: 2,
+    title: "İSTANBUL TURU",
+    date: "16.01.2023 - 20.01.2023",
+    price: "858 USD",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-12/medium/1671814582_318927722_918446159123997_4786006261621954289_n.jpg",
+    link: "https://bravotravel.az/new-tours/115-stanbul-turu.html",
+  },
+  {
+    id: 3,
+    title: "ŞARM - ƏL- ŞEYX TURU",
+    date: "11.02.2023 - 17.02.2023",
+    price: "1553 USD",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-12/medium/1671814284_318778486_859201515268009_4533359742507559573_n.jpg",
+    link: "https://bravotravel.az/new-tours/114-sarm-el-seyx-turu.html",
+  },
+  {
+    id: 4,
+    title: "DUBAYDA NOVRUZ TƏTİLİ",
+    date: "17.03.2023 - 21.03.2023",
+    price: "1525 USD",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-12/medium/1671814035_318775077_534116688733795_7032595625630696824_n.jpg",
+    link: "https://bravotravel.az/new-tours/113-dubayda-novruz-tetl.html",
+  },
+  {
+    id: 5,
+    title: "ISTANBUL SEVGİLİLƏR GÜNÜ",
+    date: "13.02.2023 - 16.02.2023",
+    price: "1027 $",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-12/thumbs/1670236819_318027961_1104105636923431_9000607565790901101_n.jpg",
+    link: "https://bravotravel.az/new-tours/112-istanbul-sevgller-gunu.html",
+  },
+  {
+    id: 6,
+    title: "SEYŞEL ADALARI",
+    date: "15.04.2023 - 22.04.2023",
+    price: "4320 $",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-12/thumbs/1670236742_313744199_642864464293237_62955742268487170_n.jpg",
+    link: "https://bravotravel.az/new-tours/111-seysel-adalari.html",
+  },
+  {
+    id: 7,
+    title: "SANTORİNİ - MYKONOS",
+    date: "19.03.2023 - 24.03.2023",
+    price: "2999 $",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-12/thumbs/1670236589_317376347_1587710911660299_1135005269473721542_n.jpg",
+    link: "https://bravotravel.az/new-tours/110-santorn-mykonos.html",
+  },
+  {
+    id: 8,
+    title: "BALİ - Mistik tropik adada unudulmaz tətil",
+    date: "10.04.2023 - 17.04.2023",
+    price: "2675 AZN",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-12/thumbs/1670236495_317442380_876203460062741_5500239468445802104_n.jpg",
+    link: "https://bravotravel.az/new-tours/109-bal-mistik-tropik-adada-unudulmaz-tetil.html",
+  },
+  {
+    id: 9,
+    title: "ŞARM ƏL ŞEYX",
+    date: "18.03.2023 - 24.03.2023",
+    price: "2340 USD",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-12/thumbs/1670236433_316420244_135306575981074_7140874793604679837_n.jpg",
+    link: "https://bravotravel.az/new-tours/108-sarm-el-seyx.html",
+  },
+  {
+    id: 10,
+    title: "GOA",
+    date: "10.01.2023 - 16.01.2023",
+    price: "2570 AZN",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-12/thumbs/1670236336_317088838_135032282675170_4362932843425191754_n.jpg",
+    link: "https://bravotravel.az/new-tours/107-goa.html",
+  },
+  {
+    id: 11,
+    title: "TBİLİSİ - Yeni il",
+    date: "28.12.2022 - 02.01.2023",
+    price: "2581 $",
+    image:
+      "https://bravotravel.az/uploads/posts/2022-12/thumbs/1670236213_316646683_722930792529591_3701633023910475299_n.jpg",
+    link: "https://bravotravel.az/new-tours/106-tbls-yeni-il.html",
+  },
+];
 const newTourCardsParent = document.querySelector("#new-tours .new-cards");
 newTourCardsParent.innerHTML = newToursElements
   .map(
-    (element) => `<article class="card">
+    (element) => `<a href="${element.link}" class="card">
           <img
-            src="${element.imageSrc}"
-            alt="${element.city}"
+            src="${element.image}"
+            alt="${element.title}"
           />
           <div class="card-info">
-            <h3>${element.city}</h3>
-            <h4>${element.time}</h4>
+            <h3>${element.title}</h3>
+            <h4>${element.date}</h4>
             <div class="card-footer">
               <p>${element.price} AZN</p>
               <span>Ətraflı</span>
             </div>
           </div>
-      </article>`,
+      </a>`,
   )
   .join("");
 
 // ! Set country tours cards
-const countryToursElements = [];
+const countryToursElements = [
+  {
+    id: 0,
+    title: "Maldiv",
+    description: "Bir yerüzü cənnəti",
+    image:
+      "https://i0.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/11/IMG_5764.jpg?resize=1000%2C1500&ssl=1",
+    link: "https://bravotravel.az/maldiv/",
+  },
+  {
+    id: 1,
+    title: "İstanbul",
+    description: "Tarix yazan şəhər",
+    image: "https://bravotravel.az/img/Istanbul.jpg",
+    link: "https://bravotravel.az/istanbul/",
+  },
+  {
+    id: 2,
+    title: "Antalya",
+    description: "Turizmin paytaxtı",
+    image: "https://bravotravel.az/img/antalya.jpg",
+    link: "https://bravotravel.az/antalya/",
+  },
+  {
+    id: 3,
+    title: "Bodrum",
+    description: "Gücünü təbiətdən alan şəhər",
+    image: "https://bravotravel.az/img/bodrum.jpg",
+    link: "https://bravotravel.az/bodrum/",
+  },
+  {
+    id: 4,
+    title: "Kemer",
+    description: "Hekayələrin diyarı",
+    image: "https://bravotravel.az/img/kemer.jpg",
+    link: "https://bravotravel.az/kemer/",
+  },
+  {
+    id: 5,
+    title: "Kuşadası",
+    description: "Xəyalların həyata çevrildiyi yer",
+    image: "https://bravotravel.az/img/kusadasi.jpg",
+    link: "https://bravotravel.az/kushadasi/",
+  },
+  {
+    id: 6,
+    title: "Marmaris",
+    description: "Sonsuz kəşf",
+    image: "https://bravotravel.az/img/marmaris.jpg",
+    link: "https://bravotravel.az/marmaris/",
+  },
+  {
+    id: 7,
+    title: "Moskva",
+    description: "Gəz, gəz və yenə gəz",
+    image: "https://bravotravel.az/img/moskva.jpg",
+    link: "https://bravotravel.az/moscow/",
+  },
+  {
+    id: 8,
+    title: "Dubay",
+    description: "Dünyanın günəşli bucağı",
+    image: "https://bravotravel.az/img/dubay.jpg",
+    link: "https://bravotravel.az/dubai/",
+  },
+  {
+    id: 9,
+    title: "Avropa",
+    description: "İlk qərar verilən destinasiya",
+    image: "https://bravotravel.az/img/avropa.jpg",
+    link: "https://bravotravel.az/europe-tours/",
+  },
+];
 let countForCountryTours = 10;
 if (countForCountryTours > 10) {
   console.error(
     "Count cannot be greater than 10 because it breaks the design.",
   );
   countForCountryTours = 10;
-}
-for (let i = 1; i <= countForCountryTours; i++) {
-  countryToursElements.push({
-    id: i,
-    city: "Maldiv",
-    desc: "Bir yerüzü cənnəti",
-    imageSrc:
-      "https://i0.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/11/IMG_5764.jpg?resize=1000%2C1500&ssl=1",
-  });
 }
 
 const countryToursCardsParent = document.querySelector(
@@ -131,15 +380,15 @@ const countryToursCardsParent = document.querySelector(
 countryToursCardsParent.innerHTML = countryToursElements
   .map(
     (element) => `<div class="card-grid" data-id=${element.id}>
-  <a href="#">
+  <a href="${element.link}" >
     <img
-      src="${element.imageSrc}"
-      alt="${element.city}"
+      src="${element.image}"
+      alt="${element.title}"
     />
     <div class="overlay"></div>
     <div class="tour-info">
-      <h3>${element.city}</h3>
-      <p>${element.desc}</p>
+      <h3>${element.title}</h3>
+      <p>${element.description}</p>
     </div>
   </a>
 </div>`,
